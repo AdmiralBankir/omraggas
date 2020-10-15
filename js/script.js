1773f33ef413a0d4c4dc7729e1ed70e5fc2a651f
Владислав Бодин <vladislavbodin@MacBook-Pro-Vladislav.local>
2020-10-15 15:21:39 +0300

    Контролирует выпадание из контентной области

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ import {makeSpace} from './space.js';
 import {play} from './play.js';
 import {aboutAddObserver} from './about.js';
 import {initControls} from './controls.js';
+import {controlVh} from './viewport-height.js';
 
+controlVh();
 play();
 makeSpace();
 aboutAddObserver();
